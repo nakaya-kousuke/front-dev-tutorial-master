2d93cd216de80a1444156bbb4e663284aed3c66d
nakaya-kousuke <i.my.me.ode23@gmail.com>
2020-09-07 21:25:52 +0900

    05_070カリキュラム終了

--- 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
+++ 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
@@ -1,6 +1,9 @@
-const mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  // direction: 'vertical',
-  loop: true,
-  effect: 'coverflow',
-})
+document.addEventListener('DOMContentLoaded', function() {
+  const hero = new HeroSlider('.swiper-container');
+  hero.start();
+  // hero.start({delay: 2000});
+
+  // setTimeout(() => {
+  //   hero.stop();
+  // }, 5000);
+});
